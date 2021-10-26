d3523173755585c2f08a4a35986b955ef2bde72f
AyushPun <ayushpun504@gmail.com>
2021-10-26 22:32:05 +0530

    Data validation added using express-validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,5 +20,6 @@ const UserSchema = new Schema({
         default : Date.now
     }
   });
-
-  module.exports = mongoose.model('user', UserSchema)
+  const User = mongoose.model('user', UserSchema);
+  User.createIndexes()  //no duplicate data will be saved in database atall
+  module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Creating a user using : POST "/api/auth/". Doesn't require auth
-router.post('/', (req, res)=> {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
-})
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {console.log(err)
+    res.json({error: 'Please enter a unique value for email', message: err.message})});
+  }
+);
 
-module.exports = router
+module.exports = router;
