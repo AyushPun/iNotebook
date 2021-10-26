9d79252c9cbdb9d66d930ede44d835ad19797efb
AyushPun <ayushpun504@gmail.com>
2021-10-26 21:19:15 +0530

    Storing Data into the databse using mongoose model

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 //Available Routes 
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res)=> {
-    res.json([])
+//Creating a user using : POST "/api/auth/". Doesn't require auth
+router.post('/', (req, res)=> {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body)
 })
 
 module.exports = router
