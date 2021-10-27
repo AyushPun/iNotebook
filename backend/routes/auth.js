22945ce89288f05fcc1ebbd4fb449d4bd184e86e
AyushPun <ayushpun504@gmail.com>
2021-10-27 23:37:35 +0530

    Hashing passwords using bcryptjs in nodejs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //Creating a user using : POST "/api/auth/createuser". Doesn't require auth
 router.post(
@@ -21,23 +25,33 @@ router.post(
     }
     //Check whether the user with same email exists already
     try {
-    let user = await User.findOne({email: req.body.email});
-    if(user) {
-      return res.status(400).json({error: "Sorry a user with this email already exist"})
-    }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exist" });
+      }
 
-  //     .then((user) => res.json(user))
-  //     .catch((err) => {console.log(err)
-  //   res.json({error: 'Please enter a unique value for email', message: err.message})});
-  res.json(user)
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      //Create a news user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, jwtSecret);
+      // console.log(authToken);
+      res.json({authToken});
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some Error Occured")
+      res.status(500).send("Some Error Occured");
     }
   }
 );
