21504d435ce34bc29b775c198ac7a6ad2e29cd96
AyushPun <ayushpun504@gmail.com>
2022-02-02 23:40:03 +0530

    additional comments

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.use(express.json())
+app.use(express.json()) //middleware
 
 //Available Routes 
 app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,28 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const { body, validationResult } = require("express-validator");
+
+const { body, validationResult } = require("express-validator");  //npm package for validations (https://express-validator.github.io/docs/)
+
 require("dotenv").config();
+
 const jwtSecret = process.env.JWT_SECRET;
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 //Creating a user using : POST "/api/auth/createuser". Doesn't require auth
-router.post(
+router.post(  //to avoid sending data in URL (get)
+  //Endpoint
   "/createuser",
+  
+  //Checks
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
-    body("password", "Password must be atleast 5 characters").isLength({
-      min: 5,
-    }),
+    body("password", "Password must be atleast 5 characters").isLength({ min: 5 }),
   ],
+
+  //Callback async function
   async (req, res) => {
+
     //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
+
     //Check whether the user with same email exists already
     try {
       let user = await User.findOne({ email: req.body.email });
@@ -35,7 +43,7 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
 
-      //Create a news user
+      //Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
